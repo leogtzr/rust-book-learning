63df96cd76bd76a35cba9ddd51715f2d2e6bab6f
lgutierrezramirez <lgutierrezramirez@switchfly.com>
2024-03-31 11:35:01 -0600

    add catch all example

---
+++ catch-all/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let day = 9;
+    match day {
+        1 => println!("It is monday"),
+        2 => println!("It is tuesday"),
+        3 => println!("It is wednesday"),
+        4 => println!("It is thursday"),
+        5 => println!("It is friday"),
+        6 => println!("It is saturday"),
+        7 => println!("It is sunday"),
+        _ => println!("Wrong day"),
+    };
+}
