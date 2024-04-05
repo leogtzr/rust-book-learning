cab2e472f652fb77b6a15a9da9c9040810275810
lgutierrezramirez <lgutierrezramirez@switchfly.com>
2024-04-05 10:57:02 -0600

    vectors usage 1)

---
+++ vectors1/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let mut v: Vec<i32> = Vec::new();
+    v.push(1);
+    v.push(2);
+    v.push(3);
+
+    println!("V: {:?}", v);
+
+    let v2 = vec![1, 2, 3, 4, 5, 6];
+    println!("V2: {:?}", v2);
+
+    let third = v2[2];
+    println!("The third element is: {}", third);
+
+    let thirdE1: &i32 = &v[2];
+    let val = 10 * (*thirdE1);
+
+    let third: Option<&i32> = v.get(2);
+
+    println!("Arithm: {}", val);
+
+    match third {
+        None => {}
+        Some(someVal) => {
+            println!("Some value = {}", someVal);
+        }
+    }
+}
