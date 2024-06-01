72cd2a10c1e2334c0cdaa55973093d8a30581b4a
lgutierrezramirez <lgutierrezramirez@switchfly.com>
2024-06-01 14:16:32 -0600

    add simple test for minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -28,3 +28,37 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+/*
+the lifetime parameters specify which argument lifetime is connected to the
+lifetime of the return value.
+
+In this case, we indicate that the returned vector should contain string slices
+ that reference slices of the argument contents (rather than the argument query).
+ */
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
